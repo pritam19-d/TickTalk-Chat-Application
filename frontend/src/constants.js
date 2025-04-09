77498f11a53bfac727b4df1d2693fe85a081bf71
Pritam Das <125026007+pritam19-d@users.noreply.github.com>
2025-04-09 08:57:04 +0530

    Update constants.js
    Added the render URL as endpoint

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -4,4 +4,5 @@ export const UPLOAD_URL = "/api/upload";
 export const USERS_URL = "/api/users";
 export const CHATS_URL = "/api/chats";
 export const MESSAGE_URL = "/api/messages"
-export const ENDPOINT = "http://localhost:5000"
+// export const ENDPOINT = "http://localhost:5000"
+export const ENDPOINT = "https://ticktalk-chat-platform.onrender.com"
